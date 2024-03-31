1b1387e4b76e41f0395f7b36fb8df34264a214f3
RavenTheshadow <hogiathang2@gmail.com>
2024-03-31 22:29:27 +0700

    ""

---
+++ src/main/resources/static/script/Manage_Doctor/Doctor_list/HomeScript.js
@@ -0,0 +1,28 @@
+document.querySelector(".login-btn").addEventListener("click", () => {
+  window.location.href = "../Login_Page/index.html";
+});
+
+// Navigate Feature Button
+document.querySelector(".patient_info").addEventListener("click", () => {
+  window.location.href = "../Manage_Patient/List_Patient/index.html";
+});
+var swiper = new Swiper(".doctor-slider", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
