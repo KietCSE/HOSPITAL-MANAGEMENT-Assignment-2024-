3c615cd1d74b937da2cb5bc8fb203ba6fb8e9733
TuanKiet <ptkiet170104@gmail.com>
2024-04-04 10:20:18 +0700

    Thêm exception

---
+++ src/main/java/com/rs/rmk/btl_ltnc/exception/ErrorFirestore.java
@@ -0,0 +1,39 @@
+package com.rs.rmk.btl_ltnc.exception;
+
+import com.rs.rmk.btl_ltnc.model.StatusCode;
+import org.springframework.http.HttpStatus;
+
+// CLASS LIỆT KÊ CÁC KIỂU LỖI CÓ THỂ NÉM RA TỪ FIREBASE
+
+public enum ErrorFirestore {
+    DOCUMENT_NOT_FOUND(StatusCode.DOCUMENT_NOT_FOUND, "Cant find the document",HttpStatus.NOT_FOUND),
+    NOT_STORE_DATA(StatusCode.NOT_STORE_DATA, "Cant store data into firestore", HttpStatus.NOT_FOUND),
+    NOT_DELETE_DATA(StatusCode.NOT_DELETE_DATA, "Cant delete data in firestore", HttpStatus.BAD_REQUEST),
+    NOT_GET_COLLECTION_DATA(StatusCode.NOT_GET_COLLECTION_DATA, "Cant get collection in firestore", HttpStatus.NOT_FOUND),
+    NOT_CHANGE_DATA(StatusCode.NOT_CHANGE_DATA, "Cant update data in firestore", HttpStatus.BAD_REQUEST),
+    NOT_GET_DOCUMENT(StatusCode.NOT_GET_DOCUMENT, "Cant load document in firestore", HttpStatus.NOT_FOUND),
+    NOT_DELETE_COLLECTION(StatusCode.NOT_DELETE_COLLECTION, "Cant delete collection successfully", HttpStatus.BAD_REQUEST),
+    COLLECTION_NOT_FOUND(StatusCode.COLLECTION_NOT_FOUND, "Cant find collection", HttpStatus.NOT_FOUND),
+    ;
+    private final StatusCode code;
+    private final String message;
+    private final HttpStatus status;
+
+    ErrorFirestore(StatusCode code, String message, HttpStatus status) {
+        this.code = code;
+        this.message = message;
+        this.status = status;
+    }
+
+    public StatusCode getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+}
---
+++ src/main/java/com/rs/rmk/btl_ltnc/model/ApiResponse.java
@@ -0,0 +1,25 @@
+package com.rs.rmk.btl_ltnc.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse <T>{
+    private StatusCode code;
+    private String message;
+    private Boolean status;
+    private T result;
+
+    public ApiResponse(StatusCode code, String message, T result, Boolean status) {
+        this.code = code;
+        this.message = message;
+        this.result = result;
+        this.status = status;
+    }
+
+    public ApiResponse() {}
+}
+
