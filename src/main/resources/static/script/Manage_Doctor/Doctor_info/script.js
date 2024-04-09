279dcac8b9ddb28bffe7fbe0586cf7c600e79f6a
Hailam2104 <lam.duong210425@hcmut.edu.vn>
2024-04-09 22:32:56 +0700

    Hiển thị danh sách bác sĩ, xem thông tin từng bác sĩ, cập nhật thông tin bác sĩ

--- src/main/resources/static/script/Manage_Doctor/Doctor_info/script.js
+++ src/main/resources/static/script/Manage_Doctor/Doctor_info/script.js
@@ -1,28 +1,62 @@
 function toggleContent() {
-  var mainContent = document.getElementById("mainContent");
-  var updateContent = document.getElementById("updateContent");
+    var mainContent = document.getElementById("mainContent");
+    var updateContent = document.getElementById("updateContent");
 
-  if (mainContent.style.display === "none") {
-    mainContent.style.display = "block";
-    updateContent.style.display = "none";
-  } else {
-    mainContent.style.display = "none";
-    updateContent.style.display = "block";
-  }
+    if (mainContent.style.display === "none") {
+        mainContent.style.display = "block";
+        updateContent.style.display = "none";
+    } else {
+        mainContent.style.display = "none";
+        updateContent.style.display = "block";
+    }
 }
 
-var in1 = document.getElementById("input_bangCap");
-var in2 = document.getElementById("input_chuyenKhoa");
-var in3 = document.getElementById("input_linhVuc");
+//getData
+fetch("http://localhost:8080/api/doctor/info?doctorName=" + sessionStorage.getItem('doctorNameCode'))
+    .then(response => response.json())
+    .then(data => {
+        addInfo(data);
+        addInfoUpdate(data);
+    })
+    .catch(err => console.log(err));
 
-in1.addEventListener("input", function () {
-  document.getElementById("bangCap").innerText = in1.value;
-});
+function addInfo(doctor) {
+    document.querySelector('.name').innerText = doctor.doctorName;
+    document.querySelector('.yearOfBirth').innerText = doctor.yearOfBirth;
+    document.querySelector('#university').innerText = doctor.university;
+    document.querySelector('#departmentName').innerText = doctor.departmentName;
+    document.querySelector('#major').innerText = doctor.major;
+    console.log(doctor);
+}
+function addInfoUpdate(doctor) {
+    document.querySelector('#name-in-update').value = doctor.doctorName;
+    document.querySelector('#yearOfBirth-in-update').value = doctor.yearOfBirth;
+    document.querySelector('#university-in-update').value = doctor.university;
+    document.querySelector('#departmentName-in-update').value = doctor.departmentName;
+    document.querySelector('#major-in-update').value = doctor.major;
+}
 
-in2.addEventListener("input", function () {
-  document.getElementById("chuyenKhoa").innerText = in2.value;
-});
-
-in3.addEventListener("input", function () {
-  document.getElementById("linhVuc").innerText = in3.value;
-});
+let updateBtn = document.getElementById('update');
+updateBtn.addEventListener('click', () => {
+    let doctorUpdate = {
+        doctorNameCode: sessionStorage.getItem('doctorNameCode'),
+        university: document.querySelector('#university-in-update').value,
+        departmentName: document.querySelector('#departmentName-in-update').value,
+        major: document.querySelector('#major-in-update').value
+    }
+    fetch("http://localhost:8080/api/doctor/update", {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(doctorUpdate)
+    })
+        .then(response => response.json())
+        .then(data => {
+            document.querySelector('#university').innerText = data.university;
+            document.querySelector('#departmentName').innerText = data.departmentName;
+            document.querySelector('#major').innerText = data.major;
+            alert("Cập nhật thành công :)")
+        })
+        .catch(err => console.log(err));
+})
