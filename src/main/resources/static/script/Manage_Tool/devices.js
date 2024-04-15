28907c4424b7d7eda192e8fd6b33b12b1ea1ba54
Nguyen Doan Minh Tam <emchuaconguoiyeu1@gmail.com>
2024-04-15 14:08:32 +0700

    thêm tên user

--- src/main/resources/static/script/Manage_Tool/devices.js
+++ src/main/resources/static/script/Manage_Tool/devices.js
@@ -1,3 +1,11 @@
+username = sessionStorage.getItem('Username');
+if (username !== "" && username != null) {
+    document.querySelector('.header .login-btn a').innerText = username;
+}
+else {
+    document.querySelector('.header .login-btn a').innerText = "Tài khoản";
+}
+
 fetch('/api/devices/getFullDevices', {
     method: 'GET',
     headers: {
